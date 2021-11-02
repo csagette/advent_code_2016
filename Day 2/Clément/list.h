3b5bcd737e2544139c53fed0b7ecb41052395f26
csagette <90904578+csagette@users.noreply.github.com>
2021-11-02 23:00:09 +0100

    commit on 'main'

---
+++ Day 2/Clément/list.h
@@ -0,0 +1,18 @@
+#ifndef LIST_H
+#define LIST_H
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct List List;
+struct List {
+    int value;
+    List *next;
+};
+
+List *insert (List *list, int e);
+List *append (List *list, int e);
+List *reverse (List *list);
+void print_list(List *list);
+
+
+#endif
